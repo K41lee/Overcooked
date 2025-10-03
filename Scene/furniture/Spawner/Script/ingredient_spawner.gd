94d18febe388c2f723e01d3656d97925ae7e7657
AnthonyH <hommais.anthony@gmail.com>
2025-10-03 16:54:51 +0200

    Spawn sans erreur

--- Scene/furniture/Spawner/Script/ingredient_spawner.gd
+++ Scene/furniture/Spawner/Script/ingredient_spawner.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
-@export var ingredient_scene : PackedScene   # la scène spécifique de l’ingrédient (Tomato.tscn, Lettuce.tscn, Onion.tscn)
+@export var ingredient_scene : PackedScene
 @onready var spawn_point = $SpawnPoint
 
 func try_give_ingredient(agent: Node) -> Node2D:
-	if agent.held_item == null: # agent doit avoir les mains libres
+	if agent.held_ingredient == null:
 		var ingredient = ingredient_scene.instantiate()
 		get_parent().add_child(ingredient)
+		print(self.global_position)
+		print(spawn_point.global_position)
 		ingredient.global_position = spawn_point.global_position
+		print(ingredient.global_position)
 		agent.pickup(ingredient)
 		return ingredient
 	return null
